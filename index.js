9c138fd15746daa45950161754b3a8514de48bd6
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-10 16:14:15 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove("main")
+const newHeader = document.createElement('h1'); 
+newHeader.id = "victory";
+newHeader.innerHTML = "Jeff is the champion"
+document.body.appendChild(newHeader)
